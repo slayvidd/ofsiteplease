f07bc850ef8b88fff8db1763f102284d878a6293
slayvidd <140982975+slayvidd@users.noreply.github.com>
2023-08-04 00:36:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Chino1 } from './components/Chino1/Chino1';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <Chino1 />
+    </div>
+  );
+});
---
+++ src/components/Chino1/Chino1.tsx
@@ -0,0 +1,156 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './Chino1.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 1:2 */
+export const Chino1: FC<Props> = memo(function Chino1(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.frame97}>
+        <div className={classes.frame95}>
+          <div className={classes.rectangle1}></div>
+          <div className={classes.frame94}>
+            <div className={classes.frame93}>
+              <div className={classes.frame86}>
+                <div className={classes.onlyFansMayBeOver1000JustForAS}>
+                  <div className={classes.textBlock}>
+                    OnlyFans may be over $100.00 just for a subscription, don’t worry though.
+                  </div>
+                  <div className={classes.textBlock2}>
+                    Name has over 500 Top Models from OnlyFans at discounted prices.
+                  </div>
+                  <div className={classes.textBlock3}>
+                    <p></p>
+                  </div>
+                  <div className={classes.textBlock4}>We are the top shop for your needs.</div>
+                </div>
+                <div className={classes.frame81}>
+                  <div className={classes.sUPPORT}>SUPPORT</div>
+                  <div className={classes.sOCIAL}>SOCIAL</div>
+                </div>
+                <div className={classes.frame82}>
+                  <div className={classes._1LEAKSSHOPWITH}>#1 LEAKS SHOP WITH</div>
+                  <div className={classes.tOPMODELS}>TOP MODELS</div>
+                </div>
+                <div className={classes.frame1}>
+                  <div className={classes.rectangle18}></div>
+                  <div className={classes.rectangle19}></div>
+                  <div className={classes.rectangle20}></div>
+                </div>
+                <div className={classes.browse}>Browse </div>
+                <div className={classes.frame83}>
+                  <div className={classes.rectangle182}></div>
+                  <div className={classes.rectangle192}></div>
+                  <div className={classes.rectangle202}></div>
+                </div>
+                <div className={classes.learnMore}>Learn More</div>
+                <div className={classes.rectangle12}></div>
+                <div className={classes.rectangle13}></div>
+                <div className={classes.rectangle14}></div>
+              </div>
+              <div className={classes.frame2}>
+                <div className={classes.mODELS}>MODELS</div>
+                <div className={classes.mODELS2}>MODELS</div>
+                <div className={classes._250}>250+</div>
+              </div>
+              <div className={classes.frame92}>
+                <div className={classes.fEEDBACK}>FEEDBACK</div>
+                <div className={classes._60}>60+</div>
+              </div>
+              <div className={classes.frame91}>
+                <div className={classes.mEMBERS}>MEMBERS</div>
+                <div className={classes._179}>179+</div>
+              </div>
+              <div className={classes._100}>100+</div>
+            </div>
+            <div className={classes.oRDERS}>ORDERS</div>
+          </div>
+        </div>
+        <div className={classes.frame96}>
+          <div className={classes.bUNDLE}>BUNDLE</div>
+          <div className={classes.snapchatOF}>
+            <div className={classes.textBlock5}>Snapchat + OF</div>
+            <div className={classes.textBlock6}>
+              <p></p>
+            </div>
+          </div>
+          <div className={classes.instantAccess}>
+            <div className={classes.textBlock7}>Instant Access</div>
+            <div className={classes.textBlock8}>
+              <p></p>
+            </div>
+          </div>
+          <div className={classes._340}>$34.00</div>
+          <div className={classes.sNAPCHAT}>SNAPCHAT</div>
+          <div className={classes._240}>
+            <div className={classes.textBlock9}>$24.00</div>
+            <div className={classes.textBlock10}>
+              <p></p>
+            </div>
+          </div>
+          <div className={classes.protect}></div>
+          <div className={classes.allContentExtra}>All Content + Extra</div>
+          <div className={classes.protect2}></div>
+          <div className={classes._15TBOfContent}>15 TB of Content</div>
+          <div className={classes.protect3}></div>
+          <div className={classes.customRequests}>Custom Requests</div>
+          <div className={classes.protect4}></div>
+          <div className={classes.instantAccess2}>
+            <div className={classes.textBlock11}>Instant Access</div>
+            <div className={classes.textBlock12}>
+              <p></p>
+            </div>
+          </div>
+          <div className={classes.protect5}></div>
+          <div className={classes.topCreators}>Top Creators</div>
+          <div className={classes.protect6}></div>
+          <div className={classes.trendingGirls}>Trending Girls</div>
+          <div className={classes.protect7}></div>
+          <div className={classes.weeklyUpdates}>Weekly Updates</div>
+          <div className={classes.protect8}></div>
+          <div className={classes.frame89}>
+            <div className={classes.oNLYFANS}>ONLYFANS</div>
+            <div className={classes._190}>
+              <div className={classes.textBlock13}>$19.00</div>
+              <div className={classes.textBlock14}>
+                <p></p>
+              </div>
+            </div>
+            <div className={classes.instantAccess3}>
+              <div className={classes.textBlock15}>Instant Access</div>
+              <div className={classes.textBlock16}>
+                <p></p>
+              </div>
+            </div>
+            <div className={classes.protect9}></div>
+            <div className={classes._10000Photos}>100,000+ photos</div>
+            <div className={classes.protect10}></div>
+            <div className={classes.realAmateurs}>Real Amateurs</div>
+            <div className={classes.protect11}></div>
+            <div className={classes.bigBootyLatinas}>Big Booty Latinas</div>
+            <div className={classes.protect12}></div>
+          </div>
+          <div className={classes.celebrities}>Celebrities</div>
+          <div className={classes.protect13}></div>
+          <div className={classes.frame8}>
+            <div className={classes.purchase}>Purchase</div>
+          </div>
+          <div className={classes.frame88}>
+            <div className={classes.purchase2}>Purchase</div>
+          </div>
+          <div className={classes.frame87}>
+            <div className={classes.purchase3}>Purchase</div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.logo_title_white11}></div>
+      <div className={classes.logo_title_white12}></div>
+      <div className={classes.onlyfansRip}>onlyfans.rip</div>
+    </div>
+  );
+});
